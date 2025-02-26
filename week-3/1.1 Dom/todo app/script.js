912c689f339070727c394f16f90bc82d2bf4ed7e
Nihal soni <nihalsoni0523@gmail.com>
2025-02-26 23:16:24 +0530

    created a todo app

---
+++ week-3/1.1 Dom/todo app/script.js
@@ -0,0 +1,23 @@
+// document.getElementById("addtask").addEventListener("click" , function() {
+//      const taskinput = document.getElementById("taskinput");
+//      if (taskinput.value.trim() !== ""){
+//         const li = document.createElement("li");
+//         li.innertext = taskinput.value;
+//         li.classList.add("item");
+//         li.addEventListener("click", function () {
+//             this.classList.add("removed"); // Add remove animation
+//             setTimeout(() => this.remove(), 300); // Wait for animation to complete
+//         });
+    
+//         document.getElementById("tasklist").appendChild(li);
+//         taskinput.value = "";
+
+//      }
+
+// })
+
+
+let ctr = 0;
+ function callback() {
+      console.log(ctr)
+ }
