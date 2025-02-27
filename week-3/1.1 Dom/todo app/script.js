a71dc6b88ebf29d52410966326b069d84b79f0da
Nihal soni <nihalsoni0523@gmail.com>
2025-02-27 20:47:01 +0530

    update , dom manipulation

--- week-3/1.1 Dom/todo app/script.js
+++ week-3/1.1 Dom/todo app/script.js
@@ -1,20 +1,20 @@
-// document.getElementById("addtask").addEventListener("click" , function() {
-//      const taskinput = document.getElementById("taskinput");
-//      if (taskinput.value.trim() !== ""){
-//         const li = document.createElement("li");
-//         li.innertext = taskinput.value;
-//         li.classList.add("item");
-//         li.addEventListener("click", function () {
-//             this.classList.add("removed"); // Add remove animation
-//             setTimeout(() => this.remove(), 300); // Wait for animation to complete
-//         });
+document.getElementById("addtask").addEventListener("click" , function() {
+     const taskinput = document.getElementById("taskinput");
+     if (taskinput.value.trim() !== ""){
+        const li = document.createElement("li");
+        li.innertext = taskinput.value;
+        li.classList.add("item");
+        li.addEventListener("click", function () {
+            this.classList.add("removed"); // Add remove animation
+            setTimeout(() => this.remove(), 300); // Wait for animation to complete
+        });
     
-//         document.getElementById("tasklist").appendChild(li);
-//         taskinput.value = "";
+        document.getElementById("tasklist").appendChild(li);
+        taskinput.value = "";
 
-//      }
+     }
 
-// })
+})
 
 
 let ctr = 0;
