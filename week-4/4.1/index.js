6db80d6ff88d3f15ccf65cedc73b60d35a804e5e
Nihal soni <nihalsoni0523@gmail.com>
2025-03-10 22:28:31 +0530

    learned about node js and bun js

--- week-4/4.1/index.js
+++ week-4/4.1/index.js
@@ -1,3 +1,14 @@
 import chalk from "chalk";
 
-console.log(chalk.blue("hello world"));
+console.log(chalk.red(`hello world`))
+console.log(chalk.green("This is success message"));
+console.log(chalk.blue("this is the wisses message"))
+
+
+// let firstName = "NIhal soni"
+// console.log(firstName)
+
+// function sum(num1 , num2){
+//     return num1 + num2;    
+// }
+// console.log(sum(7, 8))
